7dbfdceab6191601fe6c44173d7dd1fe6f090013
Sherwin Dapito <dapito.sherwin@yahoo.com>
2017-04-03 11:26:53 +0800

    Added Comments to authentication function

---
+++ app/model/client.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt-nodejs');
+
+var clientSchema = mongoose.Schema({
+	//Data for local authentication
+	local: {
+		//login email and password
+		email 		 	: String,
+		password 	 	: String,
+		//account info
+        isClient    	: Boolean,
+        name 		 	: String,
+	},
+	//Data for oauth using facebook
+	facebook         : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    //Data for oauth using gmail or google+
+    },
+    google           : {
+        id           : String,
+        token        : String,
+        email        : String,
+        name         : String
+    }
+});
+
+clientSchema.methods.generateHash = function(password){
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10), null);
+};
+
+clientSchema.methods.validPassword = function(password){
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+
+module.exports = mongoose.model('Client', clientSchema);
--- app/routes/trainer.js
+++ app/routes/trainer.js
@@ -10,12 +10,15 @@ query-string etc. This token is validated against the visitor's session or csrf 
 var csrfProtection = csrf();
 router.use(csrfProtection);
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This will render the Login Page, to pass the data to react use the res.json/ response.json
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 router.get('/login', (req, res) => {
     var messages = req.flash('error');
     
     //Token will be validated every login get request, 
-    //res.json({ csrfToken: req.csrfToken(), message: messages, hasErrors: messages.length > 0  });
-    res.render('accounts/trainer/login', { csrfToken: req.csrfToken(), message: messages, hasErrors: messages.length > 0  });
+    res.json({ csrfToken: req.csrfToken(), message: messages, hasErrors: messages.length > 0  });
+    //res.render('accounts/trainer/login', { csrfToken: req.csrfToken(), message: messages, hasErrors: messages.length > 0  });
 });
 
 router.post('/login', passport.authenticate('local.trainer.login', {
@@ -26,6 +29,10 @@ router.post('/login', passport.authenticate('local.trainer.login', {
     failureFlash: true
 }));
 
+
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This will render the Sign up Page, to pass the data to react use the res.json/ response.json
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 router.get('/signup', (req, res) => {
     var messages = req.flash('error');
     
@@ -42,18 +49,29 @@ router.post('/signup', passport.authenticate('local.trainer.signup', {
     failureFlash: true
 }));
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// This will render the Trainer Profile Page, to pass the data to react use the res.json/ response.json
+// The req.user is the data of user created by passport, 
+// you can access the trainer data using, user.local.email if you want to see the email of the trainer
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 router.get('/profile', isLoggedIn, (req, res) => {
-    res.render('accounts/trainer/profile.ejs', {
-        user : req.user 
-    });
+    res.json({user: req.user})
+    //res.render('accounts/trainer/profile.ejs', {
+    //    user : req.user 
+    //});
 });
 
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// You will be redirected to landing page or home page, You will be logged out and sessions will be destroyed
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 router.get('/logout', (req, res) => {
     req.logout();
     res.redirect('/');
 });
 
-// route middleware to make sure a user is logged in
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Route middleware to make sure a user is logged in
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 function isLoggedIn(req, res, next){
     if(req.isAuthenticated())
         return next();
